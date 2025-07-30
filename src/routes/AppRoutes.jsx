aaf0af338d8daeb89b296d152565aecb1e283c90
Patricia <patricianjuguna084@gmail.com>
2025-07-30 21:29:16 +0300

    Set up routes and protected routes in app Routes component

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
+import SignupPage from '../pages/SignupPage';
+import ReservationsPage from '../pages/ReservationsPage';
+import OrderPage from '../pages/OrderPage';
+import Dashboard from '../pages/Dashboard';
+import ProtectedRoute from './ProtectedRoute';
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/reservation" element={<ProtectedRoute><ReservationsPage /></ProtectedRoute>} />
+            <Route path="/order" element={<ProtectedRoute><OrderPage /></ProtectedRoute>} />
+            <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        </Routes>
+    )
+};
+
+export default AppRoutes;
